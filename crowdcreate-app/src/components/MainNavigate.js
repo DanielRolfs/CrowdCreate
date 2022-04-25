7b661e2d6c7d222b01d4021504d7c4c7a9a446c5
DanielRolfs <danielrolfs@icloud.com>
2022-04-25 14:46:24 +0200

    refactor project list and install firebase

--- crowdcreate-app/src/components/MainNavigate.js
+++ crowdcreate-app/src/components/MainNavigate.js
@@ -4,7 +4,7 @@ function MainNavigate() {
   const navigate = useNavigate()
 
   return (
-    <div className="MainNavigate w-full mt-2">
+    <div className="MainNavigate w-full pt-2 border-t-2">
       <div className="grid grid-cols-4 w-full h-50 flex items-center">
         <div><button onClick={() => navigate('/projectlist')}><img src="https://img.icons8.com/ios/50/000000/europe.png"/></button></div>
         <div><button onClick={() => navigate('/favourites')}><img src="https://img.icons8.com/ios/50/000000/like--v1.png"/></button></div>
--- crowdcreate-app/src/components/Projectlist.js
+++ crowdcreate-app/src/components/Projectlist.js
@@ -7,15 +7,35 @@ import MainNavigate from "./MainNavigate";
 function ProjectList() {
     return (
       <div className="ProjectList w-full h-full flex flex-col">
-        <div className="header h-14">
-          <h1>Projectlist</h1>
+         
+        {/* Headline */} 
+        <div className="header h-14 border-b-2 flex items-center flex justify-end">
+          <h1 className="flex-auto text-3xl">Projectlist</h1>
+          <div className="fixed w-12 text-2xl">+</div>
         </div>
-        
-        <div className="main flex-auto">
-          <div>Project 1</div>
-          <div>Project 2</div>
+          
+
+        {/* Project List */}
+
+        {/* Project */}
+        <div className="main flex-auto my-2 mx-2">
+          <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+            <div class="flex-shrink-0">
+              <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="test"/>
+            </div>
+            <div class="flex-1 bg-white p-2 flex flex-col justify-between">
+              <div class="flex-1">
+  
+                  <p class="text-2xl font-semibold text-gray-900">Uber</p>
+                  <p class="text-lg text-gray-900">Günster als jedes Taxi</p>
+
+              </div>
+              
+            </div>
+          </div>
         </div>
 
+        {/* Main Navigation */}
         <div>
           <Routes>
             <Route path="" element={<MainNavigate/>} />
