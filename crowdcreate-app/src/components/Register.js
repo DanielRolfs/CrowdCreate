76302080809642fddbe975e0c0ee2f093a29547c
DanielRolfs <danielrolfs@icloud.com>
2022-05-01 20:33:00 +0200

    finish auth

--- crowdcreate-app/src/components/Register.js
+++ crowdcreate-app/src/components/Register.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
-import { useState } from "react"
 import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth"
 import { auth } from "../.firebase.config"
+import { useState } from "react"
 
 function Register() {
 
@@ -66,7 +66,7 @@ function Register() {
                 <button onClick={register} class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Register</button>
               </div>
             
-              {user?.email}
+              {user.email}
           </div>
         </div>
       </div>
