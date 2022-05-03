ee50bf57d04567900f1b8c20fd4b0823295d4b76
DanielRolfs <danielrolfs@icloud.com>
2022-05-03 15:31:07 +0200

    display tasks

--- crowdcreate-app/src/components/Project/Tasks.js
+++ crowdcreate-app/src/components/Project/Tasks.js
@@ -1,17 +1,47 @@
 import { useParams } from "react-router-dom"
+import { db } from "../../.firebase.config"
+import { useState, useEffect } from "react"
 
+import {
+    collection,
+    onSnapshot,
+    doc,
+    addDoc,
+    deleteDoc
+} from "firebase/firestore"
 
 function Tasks() {
     const { id } = useParams()
+    const [tasks, setTasks] = useState([])
 
+    const tasksCollectionRef = collection(db, "tasks")
+     useEffect(() => {
+        onSnapshot(tasksCollectionRef, snapshot => {
+            setTasks(snapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    viewing: false,
+                    ...doc.data()
+                }
+            }))
+        })
+    }, []) 
+    console.log("Tasks",tasksCollectionRef)
+    
 
     return (
         <div>
             Tasks ohh yess
             {id}
+            <div>
+                { tasks.map((task, i) => (
+                    <div div > { task.taskName } </div>
+
+                ))}
+            </div>
         </div>
 
-            );
+    );
 }
 
 export default Tasks;
