9990c41126dd93af91b406e1b4ee0643f3d82a97
DanielRolfs <danielrolfs@icloud.com>
2022-05-06 10:43:01 +0200

    fixed

--- crowdcreate-app/src/components/Register.js
+++ crowdcreate-app/src/components/Register.js
@@ -66,7 +66,7 @@ function Register() {
                 <button onClick={register} className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Register</button>
               </div>
             
-              {user.email}
+            {/*   {user.email} */}
           </div>
         </div>
       </div>
